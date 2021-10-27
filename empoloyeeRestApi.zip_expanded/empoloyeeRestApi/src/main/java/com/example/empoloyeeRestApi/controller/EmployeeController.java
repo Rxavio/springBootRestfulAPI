8536799b022ea7f87280e5852c16e42ede118cd9
xavioo <rucahoxavi@gmail.com>
2021-10-27 06:25:03 +0200

    delete a specific employee

--- empoloyeeRestApi.zip_expanded/empoloyeeRestApi/src/main/java/com/example/empoloyeeRestApi/controller/EmployeeController.java
+++ empoloyeeRestApi.zip_expanded/empoloyeeRestApi/src/main/java/com/example/empoloyeeRestApi/controller/EmployeeController.java
@@ -1,9 +1,12 @@
 package com.example.empoloyeeRestApi.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -55,6 +58,19 @@ public class EmployeeController {
 			employee.setLastName(employeeDetails.getLastName());
 			employee.setFirstName(employeeDetails.getFirstName());		
 			return ResponseEntity.ok(this.employeeRepository.save(employee));
+		}
+		
+		//delete an employee
+		@DeleteMapping("/employees/{id}")
+		public Map<String, Boolean> deleteEmployee(@PathVariable(value = "id") Long employeeId)
+				throws ResourceNotFoundException {
+			Employee employee = employeeRepository.findById(employeeId)
+		.orElseThrow(() -> new ResourceNotFoundException("Employee not found for this id :: " + employeeId));
+			
+			this.employeeRepository.delete(employee);
+			Map<String, Boolean> response = new HashMap<>();
+			response.put("deleted", Boolean.TRUE);
+			return response;
 		}
 	
 }
