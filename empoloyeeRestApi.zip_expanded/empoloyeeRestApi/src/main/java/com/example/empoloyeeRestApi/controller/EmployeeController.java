d8c6ffc978acb8d45205f5a88037b2ecc816396a
xavioo <rucahoxavi@gmail.com>
2021-10-27 06:19:28 +0200

    Create new employee

--- empoloyeeRestApi.zip_expanded/empoloyeeRestApi/src/main/java/com/example/empoloyeeRestApi/controller/EmployeeController.java
+++ empoloyeeRestApi.zip_expanded/empoloyeeRestApi/src/main/java/com/example/empoloyeeRestApi/controller/EmployeeController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,6 +35,11 @@ public class EmployeeController {
 					.orElseThrow(() -> new ResourceNotFoundException("Employee not found for this id :: " + employeeId));
 			return ResponseEntity.ok().body(employee);
 		}
-		
+			
+		//create an employee
+		@PostMapping("/employees")
+		public Employee createEmployee(@RequestBody Employee employee) {
+			return employeeRepository.save(employee);
+		}
 	
 }
