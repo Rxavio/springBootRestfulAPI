2ab474905a9ff931db3a729718476fa4f7f623ab
xavioo <rucahoxavi@gmail.com>
2021-10-27 06:16:39 +0200

    get a specific employee

--- empoloyeeRestApi.zip_expanded/empoloyeeRestApi/src/main/java/com/example/empoloyeeRestApi/controller/EmployeeController.java
+++ empoloyeeRestApi.zip_expanded/empoloyeeRestApi/src/main/java/com/example/empoloyeeRestApi/controller/EmployeeController.java
@@ -3,10 +3,13 @@ package com.example.empoloyeeRestApi.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.empoloyeeRestApi.exception.ResourceNotFoundException;
 import com.example.empoloyeeRestApi.model.Employee;
 import com.example.empoloyeeRestApi.repository.EmployeeRepository;
 
@@ -21,5 +24,15 @@ public class EmployeeController {
 	public List<Employee> getAllEmployees() {
 		return employeeRepository.findAll();
 	}
+	
+	//get a specific employee
+		@GetMapping("/employees/{id}")
+		public ResponseEntity<Employee> getEmployeeById(@PathVariable(value = "id") Long employeeId)
+				throws ResourceNotFoundException {
+			Employee employee = employeeRepository.findById(employeeId)
+					.orElseThrow(() -> new ResourceNotFoundException("Employee not found for this id :: " + employeeId));
+			return ResponseEntity.ok().body(employee);
+		}
+		
 	
 }
