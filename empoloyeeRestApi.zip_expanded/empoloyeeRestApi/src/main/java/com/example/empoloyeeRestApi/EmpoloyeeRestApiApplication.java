483dc7d07ecc6e12c6e474eb0eb5fdce216d06cc
xavioo <rucahoxavi@gmail.com>
2021-10-27 05:51:17 +0200

    set datasource url to connect with postgres

---
+++ empoloyeeRestApi.zip_expanded/empoloyeeRestApi/src/main/java/com/example/empoloyeeRestApi/EmpoloyeeRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.empoloyeeRestApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpoloyeeRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpoloyeeRestApiApplication.class, args);
+	}
+
+}
---
+++ empoloyeeRestApi.zip_expanded/empoloyeeRestApi/src/test/java/com/example/empoloyeeRestApi/EmpoloyeeRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.empoloyeeRestApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpoloyeeRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
